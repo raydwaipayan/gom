df8e82bd3336eff821c4e90e1c8c22644e5ba86c
Dwaipayan Ray <dwaipayanray1@gmail.com>
2020-12-09 02:12:49 +0530

    dev: add process handling logic
    Signed-off-by: Dwaipayan Ray <dwaipayanray1@gmail.com>

---
+++ process/process.go
@@ -0,0 +1,83 @@
+package process
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
+// Proc wraps parameters unique to the
+// particular process and methods for manipulating
+// the process
+type Proc struct {
+	Name    string
+	Cmd     string
+	Argv    []string
+	Path    string
+	process *os.Process
+}
+
+// Start will execute the given Cmd and stores the process`
+func (proc *Proc) Start() error {
+	out := filepath.Join(proc.Path, "gom.out")
+	outfile, err := os.Create(out)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	procAttr := &os.ProcAttr{
+		Dir: proc.Path,
+		Env: os.Environ(),
+		Files: []*os.File{
+			os.Stdin,
+			outfile,
+			outfile,
+		},
+	}
+	process, err := os.StartProcess(proc.Cmd, proc.Argv, procAttr)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	proc.process = process
+	return nil
+}
+
+// Stop stops the given process. Sends a SIGKILL
+func (proc *Proc) Stop() error {
+	err := proc.process.Signal(os.Kill)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	return nil
+}
+
+// GracefullyStop stops the given process. Sends a SIGINT
+// Note that SIGINT doesn't work on windows
+func (proc *Proc) GracefullyStop() error {
+	err := proc.process.Signal(os.Interrupt)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	return nil
+}
+
+// Restart restarts the given process.
+// Exits the process gracefully with SIGINT and starts it again
+func (proc *Proc) Restart() error {
+	err := proc.GracefullyStop()
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	err = proc.Start()
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	return nil
+}
